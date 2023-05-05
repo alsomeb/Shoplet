007514dbc8a738e805ea2b9c1e5eb227ae8b96fc
alsomeb <alsomeb@gmail.com>
2023-05-05 20:30:09 +0200

    impl localStorage logic to keep track if products are marked checked in shoppinglists.

--- js/list.js
+++ js/list.js
@@ -7,6 +7,7 @@ import {
 } from './api.js';
 
 const currentId = getCurrentSavedShoppingListId();
+const cartId = `cart-${currentId}`;
 
 const unorderedListItemElement = $('.shopping-list');
 
@@ -23,13 +24,11 @@ const titleElement = $('#exampleModalLabel');
 const productName = $('#product-name');
 const amount = $('#amount');
 
-const checkBtn = $('.check-btn');
-
 const listeners = () => {
   $(document).ready(function () {
     $('.check-btn').on('click', function () {
-      const id = this.id;
-      handleMarkItemDone(id);
+      const productId = this.id;
+      handleMarkItemDone(productId);
     });
   });
 };
@@ -38,6 +37,16 @@ const handleMarkItemDone = (id) => {
   const itemId = `#product-${id}`;
   const element = $(itemId);
   $(element).toggleClass('line');
+  toggleItemSavedInLocalStorage(id);
+};
+
+const handleRenderProductIsChecked = (productId) => {
+  const storage = JSON.parse(localStorage.getItem(cartId));
+  const product = storage.find((item) => item.id === productId);
+  if (product.isChecked) {
+    const itemId = `#product-${productId}`;
+    $(itemId).addClass('line');
+  }
 };
 
 const handleRenderProductsInShoppingList = (currentId) => {
@@ -45,6 +54,8 @@ const handleRenderProductsInShoppingList = (currentId) => {
     if (data !== undefined && currentId !== null) {
       if (data.length > 0) {
         data.forEach((product) => {
+          // save products for LS - So we can save state if they have been checked or not
+          appendItemToCartStorage(product, data.length);
           unorderedListItemElement.append(`
           <li id="product-${product.id}">
             ${product.amount}x ${product.name}
@@ -52,6 +63,8 @@ const handleRenderProductsInShoppingList = (currentId) => {
               <i class="fa-solid fa-check"></i>
             </button>
           </li>`);
+
+          handleRenderProductIsChecked(Number(product.id));
         });
         handleRenderShoppingListInfo(currentId);
         listeners();
@@ -142,6 +155,55 @@ const refresh = () => {
   window.location.reload();
 };
 
+// handle save products cart to LocalStorage
+const appendItemToCartStorage = (product, sizeOfData) => {
+  const storage = JSON.parse(localStorage.getItem(cartId));
+
+  if (storage === null) {
+    const cart = [
+      {
+        id: product.id,
+        isChecked: false,
+      },
+    ];
+    localStorage.setItem(cartId, JSON.stringify(cart));
+  } else if (storage.length < sizeOfData) {
+    const cartUpd = {
+      id: product.id,
+      isChecked: false,
+    };
+
+    const newCartToSave = [...storage, cartUpd];
+    localStorage.setItem(cartId, JSON.stringify(newCartToSave));
+  }
+};
+
+const handleSaveMarkedItemChecked = (isChecked, storage, productId) => {
+  const oldArray = storage.filter((item) => item.id !== productId);
+
+  const cartUpd = {
+    id: productId,
+    isChecked: isChecked,
+  };
+  const newCartToSave = [...oldArray, cartUpd];
+
+  localStorage.setItem(cartId, JSON.stringify(newCartToSave));
+};
+
+// filtrera ut så vi har old array sen anv spread operator för att bygga nya cart med det item som var checkad blir true
+// Detta blir som en toggle effekt
+const toggleItemSavedInLocalStorage = (productId) => {
+  const storage = JSON.parse(localStorage.getItem(cartId));
+  const numberId = Number(productId);
+  const itemToSave = storage.find((item) => item.id === numberId);
+
+  if (storage !== null && itemToSave.isChecked === false) {
+    handleSaveMarkedItemChecked(true, storage, numberId);
+  } else if (itemToSave.isChecked) {
+    handleSaveMarkedItemChecked(false, storage, numberId);
+  }
+};
+
 // Listener Modal Button
 const modalBtnListener = () => {
   const modaladdbtn = $('#modal-addbtn');
@@ -159,6 +221,9 @@ const modalBtnListener = () => {
     handleErrorMessage(formIsValid);
   });
 };
+
+// TODO
+// "Finish" Button MED LISTENER --> TAR BORT LocalStorage nyckel (cartId) när man är klar med shoppingen, API --> deleteShoppingListById() --> redirectar till ../index.html
 
 handleRenderProductsInShoppingList(currentId);
 modalBtnListener();
