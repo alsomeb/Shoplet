acfaa410a9ddaf859b42b51e6063bf0d4c4248aa
alsomeb <alsomeb@gmail.com>
2023-05-09 17:15:19 +0200

    Collab: Erik
    Impl logic for validation new Shopping List

--- js/index.js
+++ js/index.js
@@ -8,6 +8,16 @@ const shoppingListsElement = $('#shopping_lists');
 
 const indexHeaderElement = $('.index-header-text');
 
+const addListButton = $('#add-list');
+
+const inputElementShoppingList = $('#list-name');
+
+const inputElementDate = $('#date');
+
+const titleElement = $('#exampleModalLabel');
+
+const modalAddButton = $('#exampleModal');
+
 const listeners = () => {
   $('.shopping').click(function () {
     let listId = $(this).prop('id');
@@ -76,4 +86,72 @@ const handleRenderCards = () => {
   });
 };
 
+const modalListenr = () => {
+  addListButton.click(function () {
+    modalAddButton.modal('show');
+  });
+};
+
+// Checks if all bools in array are true
+const handleValidateFields = (arrayOfBools) => {
+  let fieldsAreValid = arrayOfBools.every((item) => item === true);
+  return fieldsAreValid;
+};
+
+const handleResetModal = () => {
+  titleElement.text('New Shopping List');
+  titleElement.css('color', 'black');
+  inputElementShoppingList.css('border-color', '#ced4da');
+  inputElementDate.css('border-color', '#ced4da');
+  inputElementShoppingList.val('');
+  inputElementDate.val('');
+
+  // Close Modal
+  $('#exampleModal').modal('hide');
+};
+
+// Handles Error msg if there are any else POST new Product To API
+const handleErrorMessage = (formIsValid) => {
+  if (formIsValid) {
+    console.log('FORM IS VALID');
+    handleResetModal();
+  } else {
+    handleErrorAnimation();
+  }
+};
+
+// Animation + Text Style For Error in Modal Title
+const handleErrorAnimation = () => {
+  titleElement.text('Check Fields');
+  titleElement.css('color', 'red');
+  inputElementShoppingList.css('border-color', 'red');
+  inputElementDate.css('border-color', 'red');
+  titleElement.slideUp().slideDown();
+};
+
+// Listener Modal Button
+const modalAddNewShoppingList = () => {
+  const modaladdbtn = $('#modal-addbtn');
+
+  modaladdbtn.on('click', () => {
+    const shoppingListNameValue = inputElementShoppingList.val().trim();
+    const date = inputElementDate.val();
+
+    const currDate = new Date();
+    currDate.setHours(0, 0, 0, 0); // annars fattar den inte när den jämför
+    const inputDate = new Date(date);
+
+    // Field Validation Bools
+    const isValidName = shoppingListNameValue.length >= 2 ? true : false;
+    const isValidDate =
+      inputDate.getTime() >= currDate.getTime() ? true : false;
+
+    let formIsValid = handleValidateFields([isValidName, isValidDate]);
+
+    handleErrorMessage(formIsValid);
+  });
+};
+
 handleRenderCards();
+modalAddNewShoppingList();
+modalListenr();
